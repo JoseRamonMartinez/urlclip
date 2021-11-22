2eaa88096b2a7322a61d17b75f92e26328f31082
joseramonmartinez <josera.martinez@hotmail.com>
2021-11-22 14:19:20 +0100

    cd: added heroku 4

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+let cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'www')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../www/index.html'));
+});
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+app.listen(port, () => console.log('running'));
