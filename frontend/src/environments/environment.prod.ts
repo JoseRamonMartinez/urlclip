7d409d5b201f08a8123eb8525970dd2e2acbfb2c
joseramonmartinez <josera.martinez@hotmail.com>
2021-12-25 17:53:51 +0100

    change prod url name

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ import { env } from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://api-shortener-pro.herokuapp.com',
+  serverUrl: 'https://service-shortener.herokuapp.com',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'es-ES'],
 };
